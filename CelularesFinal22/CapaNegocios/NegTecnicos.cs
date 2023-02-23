196f3afc8435a6dd8ccae08ffb3aed5bdf5359ef
Gabriel-Oyola <90019281+Gabriel-Oyola@users.noreply.github.com>
2023-02-23 11:47:51 -0300

    Add files via upload

---
+++ CelularesFinal22/CapaNegocios/NegTecnicos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using CapaDatos;
+using System.Data;
+
+namespace CapaNegocios
+{
+    public class NegTecnicos
+    {
+        AdminisTecnicos DatosObjTecnicos = new AdminisTecnicos();
+
+        public int abmTecnicos(string accion, Tecnicos objProfesional)
+        {
+            return DatosObjTecnicos.abmTecnicos(accion, objProfesional);
+        }
+        public DataSet listadoTecnicos(string cual)
+        {
+            return DatosObjTecnicos.listadoTecnicos(cual);
+        }
+    }
+}
